a7be99d0d643ae532042bb9a81944bdbb32a32a5
Amit Mourya <amourya@vsoftcorp.com>
2019-11-09 12:26:29 +0530

    add routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'pages/login',
+    pathMatch: 'full',
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+declare var require: any;
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgbDateParserFormatter, NgbModule, NgbTabset} from '@ng-bootstrap/ng-bootstrap';
+import {AppRoutingModule} from './app-routing.module';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {AppComponent} from './app.component';
 
-
-import { AppComponent } from './app.component';
+let config = require('../assets/config.json');
 
 
 @NgModule({
@@ -10,9 +16,21 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    NgbTabset
+  ],
+  bootstrap: [AppComponent],
+  exports: []
 })
-export class AppModule { }
+export class AppModule {
+  ngOnInit() {
+  }
+}
+
